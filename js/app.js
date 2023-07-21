9ae2e9b24eef4bf3ae4415a7a8ba51647517f9df
Said <shahrokh07.96@gmail.com>
2023-07-21 21:52:34 +0530

    JavaScript sign up form validation

---
+++ js/app.js
@@ -0,0 +1,137 @@
+const usernameEl = document.querySelector("#username");
+const emailEl = document.querySelector("#email");
+const passwordEl = document.querySelector("#password");
+const confirmPasswordEl = document.querySelector("#confirm-password");
+const form = document.querySelector("#signup");
+
+
+const isRequired = (value) => value === "" ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false: true;
+const isEmailValid = (email) => {
+    const regex =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
+
+const isPasswordSecure = (password) => {
+    const regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return regex.test(password);
+}
+
+
+const showError = (input, message) => {
+    const formField = input.parentElement;
+    formField.classList.remove("success");
+    formField.classList.add("error");
+    const error = formField.querySelector("small");
+    error.textContent= message;
+}
+
+const showSuccess = (input) => {
+    const formField = input.parentElement;
+    formField.classList.remove("error");
+    formField.classList.add("success");
+    const error = formField.querySelector("small");
+    error.textContent = "";
+}
+
+const checkUsername = () => {
+    let valid = false;
+    const min = 3, max = 25;
+    const username = usernameEl.value.trim();
+    if (!isRequired(username)) {
+        showError(usernameEl, "Username cannot be blank");
+    } else if(!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max}`);
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, "Email cannot be blank.");
+    } else if(!isEmailValid(email)) {
+        showError(emailEl, "Email is not valid.");
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkPassword = () => {
+    let valid = false;
+    const password = passwordEl.value.trim();
+    if(!isRequired(password)) {
+        showError(passwordEl, "Password cannot be blank.");
+    } else if (!isPasswordSecure(password)) {
+        showError(passwordEl, 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)')
+    } else {
+        showSuccess(passwordEl)
+        valid = true;
+    }
+
+    return valid;
+}
+
+const checkConfirmPassword = () => {
+    let valid = false;
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+    if (!isRequired(confirmPassword)){
+        showError(confirmPasswordEl, "Pleases enter the password again.");
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, "The password does not match.");
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+
+    return valid;
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let isUsernameValid = checkUsername(), isEmailValid = checkEmail(),
+    isPasswordValid = checkPassword(), isConfirmPasswordValid = checkConfirmPassword();
+    let isFormValid = isUsernameValid && isEmailValid
+    && isPasswordValid && isConfirmPasswordValid;
+    if(isFormValid) {
+        // submit to the server
+    }
+});
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args);
+        }, delay);
+    }
+}
+
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'username':
+            checkUsername();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'password':
+            checkPassword();
+            break;
+        case 'confirm-password':
+            checkConfirmPassword();
+            break;
+    }
+}));
+
